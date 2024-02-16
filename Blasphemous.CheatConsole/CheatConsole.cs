3551f13555f51314121b7548e33475195d083523
unknown <brandenek03912@hotmail.com>
2024-02-15 20:24:34 -0500

    core: add more documentation

--- Blasphemous.CheatConsole/CheatConsole.cs
+++ Blasphemous.CheatConsole/CheatConsole.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 namespace Blasphemous.CheatConsole;
 
-internal class CheatConsole : BlasMod
+/// <summary>
+/// Handles opening the console and registering custom commands
+/// </summary>
+public class CheatConsole : BlasMod
 {
     internal CheatConsole() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
+    /// <summary>
+    /// Register handlers
+    /// </summary>
     protected override void OnInitialize()
     {
         InputHandler.RegisterDefaultKeybindings(new Dictionary<string, KeyCode>()
