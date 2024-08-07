df91bf91e2c401aa1ed286a1899dac8b98954a43
unknown <brandenek03912@hotmail.com>
2024-08-07 18:39:38 -0400

    core: target new libs and api

--- Blasphemous.CheatConsole/Main.cs
+++ Blasphemous.CheatConsole/Main.cs
@@ -3,7 +3,7 @@
 namespace Blasphemous.CheatConsole;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
 internal class Main : BaseUnityPlugin
 {
     public static CheatConsole CheatConsole { get; private set; }
